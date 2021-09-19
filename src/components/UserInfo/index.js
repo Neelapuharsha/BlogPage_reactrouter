2674e9f09c421ba27973ea4f71110d99262377b5
Neelapuharsha <neelapuharsha@gmail.com>
2021-09-19 11:35:35 +0000

    first commit

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="profile-img"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+    />
+    <h1 className="user-name">N Harsha Vardhan Reddy</h1>
+    <p className="user-designation">Full Stack Trainee at CCBP</p>
+  </div>
+)
+
+export default UserInfo
